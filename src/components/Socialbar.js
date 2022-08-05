680e3ef411d9554b238f1ba280d62a23ba095b10
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2022-08-05 15:31:46 +1200

    Social links updated
    Social links now open in different tab.
    Font in Navbar updated

--- src/components/Socialbar.js
+++ src/components/Socialbar.js
@@ -6,31 +6,31 @@ export default function Socialbar() {
     return (
         <div class="socialbar-body">
 
-            <a href='https://instagram.com/cafedemonteverde'>
+            <a href='https://instagram.com/cafedemonteverde' target='_blank' rel='noreferrer'>
                 <div className='social-icon'>
                     <h1><BsInstagram /></h1>
                 </div>
             </a>
             
-            <a href='https://www.facebook.com/cafedemonteverde'>
+            <a href='https://www.facebook.com/cafedemonteverde' target='_blank' rel='noreferrer'>
                 <div className='social-icon'>
                     <h1><FaFacebookF /></h1>
                 </div>
             </a>
 
-            <a href='https://twitter.com/cafedemonteverd'>
+            <a href='https://twitter.com/cafedemonteverd' target='_blank' rel='noreferrer'>
                 <div className='social-icon'>
                     <h1><BsTwitter /></h1>
                 </div>
             </a>
 
-            <a href='https://www.linkedin.com/company/cafemonteverde'>
+            <a href='https://www.linkedin.com/company/cafemonteverde' target='_blank' rel='noreferrer'>
                 <div className='social-icon'>
                     <h1><FaLinkedinIn /></h1>
                 </div>
             </a>
 
-            <a href='https://www.youtube.com/channel/UCxV7672hO3-AWB2hG5K0vdg/featured'>
+            <a href='https://www.youtube.com/channel/UCxV7672hO3-AWB2hG5K0vdg/featured' target='_blank' rel='noreferrer'>
                 <div className='social-icon'>
                     <h1><FaYoutube /></h1>
                 </div>
